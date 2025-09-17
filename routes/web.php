e1242fbe3ff5d786c9921f3fb9171e1540006ef5
phana07 <phanaphorn37@gmail.com>
2025-09-17 18:56:13 +0700

    chore: bootstrap laravel app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home']);
+Route::get('/about', [PageController::class, 'about']);
+
